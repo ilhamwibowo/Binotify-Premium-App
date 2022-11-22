4269ec86232537dfe5cae8ba720df1e6feef5f70
Ilham Wibowo <13520013@std.stei.itb.ac.id>
2022-11-22 04:48:11 +0000

    Revert "Merge branch 'main' of https://gitlab.informatika.org/if3110-2022-k01-02-43/binotify-premium-app"
    This reverts commit f2f746d3910f7074339d30acd65e707cff237920

--- src/pages/Subscription.tsx
+++ src/pages/Subscription.tsx
@@ -2,17 +2,14 @@ import React from 'react'
 import "./Subscription.css"
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
-import SubscriptionRequest from '../components/SubscriptionRequest';
 
 const Subscription = () => {
   return (
     <div className="container">
       <Navbar />
       <Sidebar />
-      <div className='main-subscription'>
-        <SubscriptionRequest />
-        <SubscriptionRequest />
-        <SubscriptionRequest />
+      <div className='main'>
+        <h1>Subscription</h1>
       </div>
     </div>
   )
