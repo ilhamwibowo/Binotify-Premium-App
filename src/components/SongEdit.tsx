1029c90964d0f273fc4e6a003b31c27c1b92aca4
azmialfatihs <13520158@std.stei.itb.ac.id>
2022-12-02 14:54:11 +0700

    feat: delete

--- src/components/SongEdit.tsx
+++ src/components/SongEdit.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios';
 import './SongEdit.css'
 
 interface ManageMusic{
@@ -15,6 +16,21 @@ interface Props {
 }
 
 const SongEdit: React.FC<Props> = ({song, songList, setSongList }) => {
+
+    const handleDelete = async (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        const songData ={
+            song_id: song.song_id,
+        };
+    
+        try {
+          const { data } = await axios.post("http://localhost:8000/song/delete", songData);
+          window.location.reload();
+        } catch (e) {
+          alert(e);
+        }
+      }
+
     return(
         <div className='song-edit'>
             <form className='grid'>
@@ -23,7 +39,7 @@ const SongEdit: React.FC<Props> = ({song, songList, setSongList }) => {
                 <input type="text" className='input-lagu' id="penyanyi" defaultValue={song.penyanyi} />
                 <input type="text" className='input-lagu' id="audio-path" defaultValue={song.audio_path} />
                 <button className='btn-edit'>Edit</button>
-                <button className='btn-delete'>Delete</button>
+                <button onClick={handleDelete} className='btn-delete'>Delete</button>
             </form>
         </div>
     )
